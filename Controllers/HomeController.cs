6b847d91d621522e944b6d461100f9207cf0d485
Lucas Moraes <ls470072@byu.edu>
2026-02-08 14:44:39 -0700

    Mission 5: Layout polish and calculator validation.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using SoccerHobby.Models;
 
 namespace SoccerHobby.Controllers
 {
@@ -11,20 +9,9 @@ namespace SoccerHobby.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Calculator()
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
-}
-
-public IActionResult Calculator()
-{
-    return View();
 }
---
+++ wwwroot/js/calculator.js
@@ -0,0 +1,27 @@
+﻿$(function () {
+    const RATE_PER_HOUR = 35.0;
+
+    function showError(show) {
+        $("#hoursError").toggleClass("d-none", !show);
+    }
+
+    function formatMoney(amount) {
+        return amount.toLocaleString("en-US", { style: "currency", currency: "USD" });
+    }
+
+    $("#calcBtn").on("click", function () {
+        const hoursRaw = $("#hours").val();
+        const hours = parseFloat(hoursRaw);
+
+        if (isNaN(hours) || hours <= 0) {
+            showError(true);
+            $("#total").val("");
+            return;
+        }
+
+        showError(false);
+
+        const total = hours * RATE_PER_HOUR;
+        $("#total").val(formatMoney(total));
+    });
+});
